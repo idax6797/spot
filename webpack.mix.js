0e5095777281d285de6d3b116388de0cb202fdad
idax6797 <64838676+idax6797@users.noreply.github.com>
2021-11-12 08:39:32 +0100

    setup wordpress/scsss

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,12 @@
 
 let mix = require('laravel-mix');
 
-mix.js('src/app.js', 'dist')
-   .sass('src/app.scss', 'dist');
+mix
+.options({
+   processCssUrls:false,
+})
+.js('src/app.js', 'dist')
+.sass('src/app.scss', 'dist');
+
+
+
