ec80f022b926e04c5371e674c636de4ec4a007b2
idax6797 <64838676+idax6797@users.noreply.github.com>
2021-11-26 15:12:50 +0100

    Projekter

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
     <title>SPOT</title>
 
     <?php wp_head();?>
