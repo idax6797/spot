1f0ed315978f16ab2fdf5e1c4f90a959b6e82b68
idax6797 <64838676+idax6797@users.noreply.github.com>
2021-11-30 11:18:05 +0100

    SPOT på midten if statement, så der jeg vises forskelligt indhold på de to kategorier

--- archive.php
+++ archive.php
@@ -1,14 +1,35 @@
 <?php get_header();?>
 
 
-<div class="content archive">
 
-    <h1><?php single_cat_title();?></h1>
-    <h2><?php the_archive_description();?></h2>
-    <iframe class="video-projects" src="https://www.youtube.com/embed/iQosI9gM18g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <p>SPOT er meget mere end en festival. Se hvilke projekter vi laver resten af året.</p>
-      <?php if (have_posts()) : while(have_posts()) : the_post();?>
-      
+  <?php
+  // Get the ID of a given category
+  $category_id = get_cat_ID( 'SPOT på Midten' );
+  // Get the URL of this category
+  $category_link = get_category_link( $category_id );
+  ?>
+
+<?php if ( is_category( 'Projekter' )) : ?>
+
+<div class="content archive">
+  <h1><?php single_cat_title();?></h1>
+  <h2><?php the_archive_description();?></h2>
+  <iframe class="video-projects" src="https://www.youtube.com/embed/iQosI9gM18g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <p>SPOT er meget mere end en festival. Se hvilke projekter vi laver resten af året.</p>
+
+    <div class="category-pa-midten">
+      <img src="<?php the_post_thumbnail_url('smallest');?>">
+      <div class="container-category">
+        <h3><?php echo get_cat_name( $category_id = 6 )?></h3>
+          <div class="container-category__info">
+            <p><?php echo category_description(6);?></p>
+            <a href="<?php echo esc_url( $category_link ); ?>" title="SPOT på midten">Køb billet</a>
+          </div>
+      </div>
+    </div>
+
+    <?php if (have_posts()) : while(have_posts()) : the_post();?>
+
       <div class="blogs">
           <?php if(has_post_thumbnail()):?>
             <img src="<?php the_post_thumbnail_url('smallest');?>">
@@ -16,22 +37,43 @@
           
           <div class="blogs__content">
             <h3><?php the_title();?></h3>
-           
+            
             <div class="blog__excerpt">
               <p><?php the_excerpt();?></p>
-              <a href="<?php the_permalink()?>"> Læs mere </a> 
+              <a href="<?php the_permalink()?>"> Køb billet </a> 
             </div>
           </div>
       </div>
+  </div>
+  <?php endwhile; endif;?>
+
+  <?php else : ?>
+    <div class="content archive">
+      <h1><?php single_cat_title();?></h1>
+      <h2><?php the_archive_description();?></h2>
+      <iframe class="video-projects" src="https://www.youtube.com/embed/_qmu0SslyvI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>      <p>SPOT på Midten yay</p>
+
+      <?php if (have_posts()) : while(have_posts()) : the_post();?>
+      <div class="blogs">
+        <?php if(has_post_thumbnail()):?>
+          <img src="<?php the_post_thumbnail_url('smallest');?>">
+        <?php endif;?>
         
-    <?php endwhile; endif;?>
-
-    <p>Øvrige projekter</p>
-
+        <div class="blogs__content">
+          <h3><?php the_title();?></h3>
+          
+          <div class="blog__excerpt">
+            <p><?php the_excerpt();?></p>
+            <a href="<?php the_permalink()?>"> Køb billet </a> 
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php endwhile; endif;?>
+  <?php endif;  ?>
+  
 
 </div>
-
-
 
 
 <?php get_footer();?>
--- front-page.php
+++ front-page.php
@@ -2,7 +2,9 @@
 
 <div class="content front-page">
 
-    <img class="spot-festival-logo"src="http://localhost:8888/spot/wp-content/uploads/2021/11/landingpage-spot-logo.png" alt="SPOT festival 2022">
+   <h1><?php the_title();?></h1> 
+   
+   <img class="spot-festival-logo"src="http://localhost:8888/spot/wp-content/uploads/2021/11/landingpage-spot-logo.png" alt="SPOT festival 2022">
 
     <div class="container-count">
         <p class="countdown" id="demo"></p>
@@ -11,8 +13,6 @@
         <p>Minutter</p>
         <p>Sekunder</p>
     </div>
-
-    <h2><?php the_title();?></h2>
 
     <?php if (have_posts()) : while(have_posts()) : the_post();?>
 
